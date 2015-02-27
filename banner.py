eb2ecd84e15f3dc1765ed452870cc86d23a5106b
Maxim Krivodaev <maxim.krivodaev@gmail.com>
2015-02-28 02:46:33 +0300

    Code refactor. Write some tests.

--- banner.py
+++ banner.py
@@ -4,12 +4,30 @@ __project__ = 'banner'
 
 
 class Banner(object):
+    """
+    >>> Banner(['http://', 1, 'none']) # doctest: +ELLIPSIS
+    <banner.Banner object at 0x...>
+    """
     def __init__(self, config):
         self.__image_url = config.pop(0)
         self.show_count = int(config.pop(0))
-        self.categories = config
+        self.categories = set(config)
 
     @property
     def image_url(self):
+        """
+        >>> b = Banner(['http://', 1, 'none'])
+        >>> b.show_count
+        1
+        >>> b.image_url
+        'http://'
+        >>> b.show_count
+        0
+        >>> b.image_url
+        '/images/404.jpg'
+        """
+        if not self.show_count:
+            return '/images/404.jpg'
+
         self.show_count -= 1
         return self.__image_url
--- server.py
+++ server.py
@@ -1,52 +1,27 @@
 # coding=utf-8
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import csv
-from random import randint
 from os.path import curdir, sep
 from urlparse import parse_qsl
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 from banner import Banner
 
 HOST = '127.0.0.1'
 PORT = 8080
 
-CONFIG = {
-    'banners': [],
-    'count': 0,
-}
+CONFIG = None
 
 
 class Handler(BaseHTTPRequestHandler):
 
-    def __get_category(self):
-        categories = [value for k, value in parse_qsl(self.path)]
-        if not categories:
-            return None
-
-        return categories[randint(0, len(categories))]
-
-    def __get_banner(self):
-        banners = CONFIG['banners']
-        category = self.__get_category()
-        if category is None:
-            banner = banners[randint(0, CONFIG['count'])]
-        else:
-            same = filter(lambda x: category in x.categories, banners)
-            banner = same[randint(0, len(same) - 1)]
-
-        return banner if banner.show_count > 0 else None
-
-    def __send(self, mime):
+    def __send(self, mime, categories):
         f = open(curdir + sep + self.path)
         self.send_response(200)
         self.send_header('Content-type', mime)
         self.end_headers()
         result = f.read()
         if mime == 'text/html':
-            banner = None
-            while banner is None:
-                banner = self.__get_banner()
-
+            banner = get_banner(parse_qsl(categories))
             result = result.format(image_url=banner.image_url)
 
         self.wfile.write(result)
@@ -54,6 +29,7 @@ class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         mime = 'text/html'
+        categories = self.path
         if self.path == '/' or self.path.startswith('/?'):
             self.path = 'index.html'
 
@@ -66,24 +42,67 @@ class Handler(BaseHTTPRequestHandler):
                 send_replay = True
 
             if send_replay:
-                self.__send(mime)
+                self.__send(mime, categories)
 
         except IOError:
             self.send_error(404, 'Page not found: {0}'.format(self.path))
 
 
+def get_banner(categories):
+    """
+    Don't understand how to use globals
+    """
+    if not len(CONFIG):
+        return None
+
+    # Get requested categories
+    categories = set([value for k, value in categories[:]])
+    # Make intersection with banners categories
+    banner_intersection_length = [
+        {'index': i, 'intersections': len(list(categories.intersection(b1.categories)))}
+        for i, b1 in enumerate(CONFIG)
+    ]
+    # Sort by max intersections
+    sorted_banners = sorted(
+        banner_intersection_length, key=lambda b2: b2['intersections'], reverse=True
+    )
+    # Filter by max intersection
+    max_intersection = sorted_banners[0]['intersections']
+    filtered_banners = filter(lambda b: b['intersections'] == max_intersection, sorted_banners)
+    # Sort by max Banner.show_count
+    sorted_banners = sorted(
+        filtered_banners, key=lambda b3: CONFIG[b3['index']].show_count, reverse=True
+    )
+    # Get first banner
+    banner = CONFIG[sorted_banners[0]['index']]
+    if banner.show_count == 1:
+        CONFIG.pop(sorted_banners[0]['index'])
+
+    return banner
+
+
 def read_config(file_path='config.csv'):
+    """
+    >>> read_config('test_config.csv') #doctest: +ELLIPSIS
+    [<banner.Banner object at 0x...>]
+    """
     with open(file_path, 'rb') as csv_file:
         lines = csv.reader(csv_file, delimiter=';')
-        CONFIG['banners'] = [Banner(line) for line in lines]
-        CONFIG['count'] = len(CONFIG['banners']) - 1
+        config = [Banner(line) for line in lines]
+    return config
 
 
 def run_server():
-    read_config()
+    """
+    >>> run_server()
+    Server started on 127.0.0.1:8080
+    Server stopped
+
+    Didn't find how to simulate ctrl-c
+    """
     server = HTTPServer((HOST, PORT), Handler)
     try:
-        print('Server started on port: {0}'.format(PORT))
+        print('Server started on {host}:{port}'.format(host=HOST, port=PORT))
         server.serve_forever()
     except KeyboardInterrupt:
         print('Server stopped')
@@ -91,4 +110,5 @@ def run_server():
 
 
 if __name__ == '__main__':
+    CONFIG = read_config()
     run_server()
